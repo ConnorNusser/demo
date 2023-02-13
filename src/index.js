b0bc905e523d75a94aca1b961f99d4ebdc4338a7
Connor <connornusser@gmail.com>
2023-02-12 20:05:53 -0600

    hi

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 function Square(props) {
     return (
@@ -15,7 +34,7 @@ function Square(props) {
     constructor(props) {
         super(props);
         this.state = {
-          squares: Array(9).fill(".."),
+          squares: Array(9).fill(""),
           xIsNext: false,
         };
     }
@@ -32,7 +51,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
   
       return (
         <div>
